7b0b298a5d4b2e0cfcfb5750216441d60dcdce6e
student <student@vuz.magtu.ru>
2023-04-14 11:46:19 +0500

    script

--- arrUrl/index.php
+++ arrUrl/index.php
@@ -7,8 +7,11 @@ preg_match_all($pattern, $html, $matches);
 // echo '<pre>';
 // var_dump($matches);
 // echo '</pre>';
-echo $matches[1][0];
-// foreach($matches as $value){
-//     echo $value;
-// }
+// echo $matches[1][0];
+
+foreach($matches[1] as $value){
+    echo $value.'<br>';
+    $local_path = __DIR__."\dd\dd.xlsx";
+    copy($value, $local_path);
+}
 // echo $html;
